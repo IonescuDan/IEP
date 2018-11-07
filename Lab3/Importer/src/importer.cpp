63a2bcdbc1c1e88f12a07c2629269b0ae0ea9ce3
IonescuDan <danni.inc9610@gmail.com>
2018-11-07 09:32:25 +0200

    IEP lab 3

---
+++ Lab3/Importer/src/importer.cpp
@@ -0,0 +1,6 @@
+#include "fisier.h"
+
+int main(int argc, char *argv[]){
+    function();
+    return 0;
+}
---
+++ Lab3/Library/build/CMakeFiles/fisier.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fisier.dir/src/fisier.cpp.o"
+  "libfisier.pdb"
+  "libfisier.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fisier.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
